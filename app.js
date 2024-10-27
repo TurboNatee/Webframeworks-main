3855e3e8d18092b57797de31d855f570a3b50690
TurboNatee <nathan.swanepoel@students.ittralee.ie>
2024-10-27 17:05:42 +0000

    On page load it redirects to the Homepage

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var apiRouter = require('./app_api/routes/index'); // Import your API routes
 
 var app = express();
 
-
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
@@ -19,8 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// Redirect to homepage on root access
+app.get('/', (req, res) => {
+  res.redirect('/Homepage'); //
+});
 
+app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
